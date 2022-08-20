df2f06467d577ef366b8993e2c2f72441602ded8
肖阳 <xiaoyang@xiaoyangdeMacBook-Pro.local>
2022-08-20 15:58:44 +0800

    login

--- pages/login/login.js
+++ pages/login/login.js
@@ -1,3 +1,4 @@
+const md5 = require('../../assets/md5');
 Page({
   data: {
     phone: '',
@@ -27,16 +28,55 @@ Page({
         duration: 2000
       })
     } else {
-      // 跳转至index页面
-      wx.switchTab({
-        url: '/pages/home/home'
+      wx.request({
+        // url: 'http://60.205.57.246:8088/dlwd-auth/oauth/token',
+        url: 'http://60.205.57.246:8088/dlwd-auth/oauth/token?password='+md5.hexMD5(this.data.password)+'&tenantId=000000&username='+this.data.phone+'&grant_type=password&scope=all&type=account',
+        method: 'POST',
+        header: {
+          'content-type': 'application/json', // 默认值
+          'Authorization': 'Basic ZGx3ZF9jbGllbnQ6ZGx3ZF90b290aF9zZWNyZXQ='
+        },
+        // data: {
+        //   "username": this.data.phone,
+        //   "password": md5.hexMD5(this.data.password),
+        //   "tenantId":"000000",
+        //   "grant_type":"password",
+        //   "scope":"all",
+        //   "type": "account",
+        //   "Authorization": "Basic ZGx3ZF9jbGllbnQ6ZGx3ZF90b290aF9zZWNyZXQ="
+        // },
+        success(res) {
+          console.log(res);
+          if (res.statusCode == 200) {
+            // 跳转至index页面
+            wx.switchTab({
+              url: '/pages/home/home'
+            })
+            // 这里修改成跳转的页面 
+            wx.showToast({
+              title: '登录成功',
+              icon: "none",
+              duration: 2000
+            });
+          } else {
+            wx.showToast({
+              title: res.data.error_description,
+              icon: "none",
+              duration: 2000
+            })
+          }
+        },
+        fail () {
+          wx.hideLoading({
+            success: (res) => {
+              wx.showToast({
+                title: '网络请求失败',
+                icon: "none"
+              })
+            },
+          })
+        }
       })
-      // 这里修改成跳转的页面 
-      wx.showToast({
-        title: '登录成功',
-        icon: 'success',
-        duration: 2000
-      });
     }
   }
 }) 
